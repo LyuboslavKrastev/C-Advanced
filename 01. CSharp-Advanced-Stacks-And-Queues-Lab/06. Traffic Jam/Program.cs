55fb7327c46efe238e68fa357a89a468f7f31378
LyuboslavKrastev <34550342+LyuboslavKrastev@users.noreply.github.com>
2018-01-29 09:10:40 +0200

    Add files via upload

---
+++ 01. CSharp-Advanced-Stacks-And-Queues-Lab/06. Traffic Jam/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carsThatPass = int.Parse(Console.ReadLine());
+            var jam = new Queue<string>();
+            var passed = 0;
+            string input;
+            while ((input=Console.ReadLine()) != "end")
+            {
+                if (input == "green")
+                {
+                    var jamCheck = Math.Min(carsThatPass, jam.Count);
+                    for (int i = 0; i < jamCheck; i++)
+                    {
+                        Console.WriteLine($"{jam.Dequeue()} passed!");
+                        passed++;
+                    }                  
+                    continue;
+                }
+                jam.Enqueue(input);
+            }
+
+            Console.WriteLine($"{passed} cars passed the crossroads.");
+        }
+    }
+}
